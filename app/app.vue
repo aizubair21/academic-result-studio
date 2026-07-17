261487364580ff035393d68c16758a4eff6f0dde
aizubair21 <amimulit21@gmaill.com>
2026-07-17 16:04:28 +0600

    Init Academic Result Studio Repo

---
+++ app/app.vue
@@ -0,0 +1,13 @@
+<script lang="ts" setup>
+
+</script>
+
+<template>
+  <NuxtPage>
+    
+  </NuxtPage>
+</template>
+
+<style lang="postcss" scoped>
+  
+</style>
---
+++ app/pages/index.vue
@@ -0,0 +1,11 @@
+<script lang="ts" setup>
+
+</script>
+
+<template>
+    <h1 class="flex justify-center items-center h-screen w-full py-2 px-4 border text-3xl capitalize"> welcome to result management engine</h1>
+</template>
+
+<style lang="postcss" scoped>
+    
+</style>
